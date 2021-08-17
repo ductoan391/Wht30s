9a5ffb0495ca9ee63f6ac3d092f109fe0a4e0ebe
Duc Toan <ductoan391@gmail.com>
2021-08-17 22:22:13 +0700

    firt commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './Components/header-components/header/header.component';
+import { BestProductComponent } from './Components/header-components/best-product/best-product.component';
+import { ProductComponent } from './Components/body/product/product.component';
+import { BrandComponent } from './Components/body/brand/brand.component';
+import { IssueComponent } from './Components/body/issue/issue.component';
+import { FooterComponent } from './Components/footer/footer/footer.component';
+import { FooterMenuComponent } from './Components/footer/footer-menu/footer-menu.component';
+import { MenuComponent } from './Components/header-components/menu/menu.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    BestProductComponent,
+    ProductComponent,
+    BrandComponent,
+    IssueComponent,
+    FooterComponent,
+    FooterMenuComponent,
+    MenuComponent,
+
   ],
   imports: [
     BrowserModule,
