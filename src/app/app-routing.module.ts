cfbc37fcf69ef8adb9a5719f0e776e4c721e5a35
Duc Toan <ductoan391@gmail.com>
2021-08-18 12:48:45 +0700

    login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { UserComponent } from './Components/header-components/user/user.component';
+import { HomeComponent } from './Components/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: "login", component: UserComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,7 +11,15 @@ import { IssueComponent } from './Components/body/issue/issue.component';
 import { FooterComponent } from './Components/footer/footer/footer.component';
 import { FooterMenuComponent } from './Components/footer/footer-menu/footer-menu.component';
 import { MenuComponent } from './Components/header-components/menu/menu.component';
-
+import { UserComponent } from './Components/header-components/user/user.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import { HomeComponent } from './Components/home/home.component';
 
 @NgModule({
   declarations: [
@@ -24,11 +32,20 @@ import { MenuComponent } from './Components/header-components/menu/menu.componen
     FooterComponent,
     FooterMenuComponent,
     MenuComponent,
+    UserComponent,
+    HomeComponent,
 
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FlexLayoutModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatCardModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
